6e1127aa772a0c46412941149027e9dd6c8998a9
imlab <kts8559@gmail.com>
2021-04-13 15:30:37 +0900

    Modify can_test.py

--- can_test.py
+++ can_test.py
@@ -12,38 +12,31 @@ can_bus = can.interface.Bus('can0', bustype='socketcan')
 esp = 0
 sas = 0
 whl = 0
-st = time.time()
+esp_frame_id = 0.0
+sas_frame_id = 0.0
+whl_frame_id = 0.0
+for message in db.messages:
+    if message.name == 'ESP12':
+        esp_frame_id = message.frame_id
+    elif message.name == 'SAS11':
+        sas_frame_id = message.frame_id
+    elif message.name == 'WHL_SPD11':
+        whl_frame_id = message.frame_id
+
 while(True):
     try:
         message = can_bus.recv()
-        # print(message.data)
-        if message.arbitration_id == 0x220:
-            esp = db.decode_message('ESP12', message.data)['CYL_PRES']
+        if message.arbitration_id == esp_frame_id:
+            esp = db.decode_message(mesage.arbitration_id, message.data)['CYL_PRES']
 
-        elif message.arbitration_id == 0x2b0:
-            sas = db.decode_message('SAS11', message.data)['SAS_Angle']
+        elif message.arbitration_id == sas_frame_id:
+            sas = db.decode_message(message.arbitration_id, message.data)['SAS_Angle']
         
-        elif message.arbitration_id == 0x386:
-            whl = db.decode_message('WHL_SPD11', message.data)['WHL_SPD_FL']
+        elif message.arbitration_id == whl_frame_id:
+            whl = db.decode_message(message.arbitration_id, message.data)['WHL_SPD_FL']
 
         print("ESP : {:08.5f},  SAS : {:08.5f},  WHL {:08.5f}".format(esp, sas, whl))
-        # en = time.time()
-        # print(en - st)
-        # st = time.time()
-        # print(db.decode_message('ESP12', message.data))
-        # print(db.decode_message('WHL_SPD11', message.data))
-        # print(esp.decode(message.data)['CYL_PRES'])
         # time.sleep(0.001)
         # break
     except:
         pass
-    # time.sleep(1)
-    # message = can_bus.recv()
-
-# import canlib
-# from canlib import kvadblib
-# from canlib import canlib
-# import can
-
-# db = kvadblib.Dbc(filename="/media/imlab/62C1-3A4A/AE_PE_C_C_KOOKMIN_2/AE_PE_C_C_KOOKMIN_2.dbc")
-# can_bus = can.interfaces.kvaser.canlib.KvaserBus(0)
