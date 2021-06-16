4fb71b1c2cee02d8179b7b3279ece310c6b230ea
imlab <kts8559@gmail.com>
2021-06-16 21:43:56 +0900

    test HMI

--- main/main.py
+++ main/main.py
@@ -4,33 +4,42 @@ import can
 import pyaudio
 import time
 import sys
+import pandas as pd
+import os
 
-from receive_data import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
 
-## These variables are used in receive_data.py to sync threads
-TOTAL_THREADS_NUM = 4 ## Add 1 each time a sensor is added.
-thread_count = 0
+# from receive_data import receive_CAN, receive_video, receive_audio, receive_sensor
+# from hmi import WindowClass
+# # These variables are used in receive_data.py to sync threads
+# TOTAL_THREADS_NUM = 4 ## Add 1 each time a sensor is added.
+# thread_count = 0
 
 def main():
+    from receive_data import receive_CAN, receive_video, receive_audio, receive_sensor, WindowClass
+
     check_name = 'n'
     check_odd = 'n'
     start_flag = 'n'
     
     ### General setting ###
     # Read registered driver
-    DRIVER_LIST = []    # [todo] read saved driver list
-    
+    DRIVER_LIST = ["임세준", "오기성", "박중후", "김태산", "정의석"]    # [todo] read saved driver list
+    DRIVER_NAME = "김태산"
     while check_name != 'y' :
         DRIVER_NAME = input("Enter your name : ")
         
-        if str(DRIVER_NAME) in driver_list :
-            check_name = input("Are you registerd driver, {}? [y/n]".format(str(DRIVER_NAME)))
+        if str(DRIVER_NAME) in DRIVER_LIST:
+            check_name = input("Are you registered driver, {}? [y/n]".format(str(DRIVER_NAME)))
         
         else :
             check_name = input("Are your new driver, {}? [y/n] ".format(str(DRIVER_NAME)))
             if check_name == 'y' :
                 DRIVER_LIST.append(str(DRIVER_NAME))
-                # [todo] save updated drier list
+                # [todo] save updated driver list
     
     while check_odd != 'y' :
         START_ODD = input("Enter current odd meter " )
@@ -65,18 +74,19 @@ def main():
     
     #####################
 
+    myWindow = WindowClass(DRIVER_NAME)
+    myWindow.show()
     
     ### Thread setting ###
     stop_threads = False
     workers = []
-    data_names = ['CAN', 'video', 'audio', 'sensor', 'HMI']
-    thread_functions = [receive_CAN, receive_video, receive_audio, receive_sensor, receive_HMI]
+    data_names = ['CAN', 'video', 'audio', 'sensor']
+    thread_functions = [receive_CAN, receive_video, receive_audio, receive_sensor]
     func_args = {'CAN': (),
-                 'video': (),
-                 'audio': (FORMAT, RATE, CHANNELS, CHUNK),
-                 'sensor': (),
-                 'HMI': (DRIVER_NAME)
-                 }
+                'video': (),
+                'audio': (FORMAT, RATE, CHANNELS, CHUNK),
+                'sensor': (),
+                }
     #####################
     
     ### Driver's intention check ###
@@ -98,13 +108,17 @@ def main():
         print("Wrong input! Press 'Enter'")
         terminate_signal = input()
     stop_threads = True
-    
+
     # thread terminate double check
     for worker in workers:
         worker.join()
     print("Main thread finished.")
+
+    return terminate_signal
     #####################
 
     
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    stop = main()
+    app.exec_(stop)
