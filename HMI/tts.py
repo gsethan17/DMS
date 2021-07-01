63446f6d6070cb0d67c841bbab1b98df103a5501
imlab <kts8559@gmail.com>
2021-07-01 11:33:03 +0900

    Update changes

--- HMI/tts.py
+++ HMI/tts.py
@@ -3,8 +3,11 @@ from io import BytesIO
 
 #wav_fp = BytesIO()
 
-tts = gTTS( text='현재 상태를 입력해주세요', lang='ko', slow=False )
-#tts.write_to_fp(wav_fp)
-tts.save('in.wav')
-tts1 = gTTS( text='입력이 완료 되었습니다', lang='ko', slow=False )
-tts1.save('out.wav')
+# tts = gTTS( text='현재 상태를 입력해주세요', lang='ko', slow=False )
+# #tts.write_to_fp(wav_fp)
+# tts.save('in.wav')
+# tts1 = gTTS( text='입력이 완료 되었습니다', lang='ko', slow=False )
+# tts1.save('out.wav')
+
+tts1 = gTTS( text='입력이 지연되어 무응답 상태로 입력합니다', lang='ko', slow=False )
+tts1.save('no_answer.wav')
